11ab954f7f8018008754b5dc21aa7744e7f83fa4
Bruno Chirelli <brunochirelli@gmail.com>
2024-08-19 06:52:26 -0300

    add resize observer polyfill and better syntax

--- src/components/forms/TdeeForm/TdeeForm.spec.tsx
+++ src/components/forms/TdeeForm/TdeeForm.spec.tsx
@@ -2,15 +2,7 @@ import { render, screen } from "@testing-library/react";
 import TdeeForm from "./TdeeForm";
 import { TestClientProvider } from "@/providers/TestProvider";
 
-class ResizeObserver {
-  observe() {}
-  unobserve() {}
-}
-
 describe("BMRForm", () => {
-  // @ts-ignore
-  window.ResizeObserver = ResizeObserver;
-
   it("Should render without errors", () => {
     render(<TdeeForm />, { wrapper: TestClientProvider });
 
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,7 @@
 import "@testing-library/jest-dom/vitest";
+import ResizeObserver from "resize-observer-polyfill";
+
+global.ResizeObserver = ResizeObserver;
 
 const mockQuery = {
   sex: "1",
